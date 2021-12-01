c38da97387b53a61f33b095e7fed0fb3bb69fb0c
chibah-fawzi <fawzichibah@gmail.com>
2021-12-01 22:38:48 +0100

    hard coded deployed uri

--- src/App.js
+++ src/App.js
@@ -177,7 +177,7 @@ function App() {
                         > */}
             <ul className='todoList'>
               {!loading ?
-                data.map((e, i) => {
+                [data].map((e, i) => {
                   return (
                     <li key={i} className='checkboxWrapper'>
                       <div>
